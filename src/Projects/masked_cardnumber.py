88e3ccc3685b1dd93ed9c0f5b6ae2e80a5823ce8
AnneLund <annedeveloper@hotmail.com>
2023-03-02 08:02:25 +0100

    Update masked_cardnumber.py

--- src/Projects/masked_cardnumber.py
+++ src/Projects/masked_cardnumber.py
@@ -1,14 +1,15 @@
-# Function to mask a card number
+# Funktion til maskering af kortnummer
 
 def mask_card_number(card_number):
-    # Get the first 4 and last 4 digits of the card number
+    
+    # Finder de første 4 og de sidste 4 cifre i kortnummeret
     first_four = card_number[:4]
     last_four = card_number[-4:]
 
-    # Mask the digits between the first and last 4 digits
+    # Maskerer tallene imellem de første og sidste 4 cifre i kortnummeret
     masked_digits = 'X' * (len(card_number) - 8)
 
-    # Determine the card type based on the first digit(s) of the card number
+    # Finder korttypen vha det første ciffer i kortnummeret
     if card_number.startswith('1'):
         card_type = 'VISA'
     elif card_number.startswith('5'):
@@ -18,12 +19,15 @@ def mask_card_number(card_number):
     else:
         card_type = 'Unknown'
 
-    # Return the masked card number
+    # Returnerer det maskerede kortnummer
     return f"{card_type}{first_four}{masked_digits}{last_four}"
 
-
+# Kortnummer
 my_card_number = "1234567887654321"
+
+# Funktionen
 masked_card_number = mask_card_number(my_card_number)
+
 print(masked_card_number)
 
 
